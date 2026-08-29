04c6e8f38d7aa3963910e2336155ef1191f1c8c3
Andrei Podkovyrin <podkovyrin@gmail.com>
2026-08-29 15:46:55 +0200

    chore: update Rust toolchain and dependencies

--- crates/upgate-presentation/src/tui/components/spinner.rs
+++ crates/upgate-presentation/src/tui/components/spinner.rs
@@ -1,5 +1,5 @@
 const SPINNER: &[&str] = &["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 
-pub fn spinner_frame(spinner_tick: usize) -> &'static str {
+pub const fn spinner_frame(spinner_tick: usize) -> &'static str {
     SPINNER[spinner_tick % SPINNER.len()]
 }
